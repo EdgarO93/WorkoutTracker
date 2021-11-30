851c42a918b24511d525a339b2830fefad62b1b8
Edgar Ortega <edort93@gmail.com>
2021-11-29 22:42:23 -0600

    added route to add exercise

--- routes/api.js
+++ routes/api.js
@@ -14,7 +14,24 @@ router.post("/api/workouts", (req, res) => {
     });
   });
 
+// adding exercise
+router.put("/api/workouts/:id", (req, res) => {
 
+    Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            //The $inc operator increments a field by a specified value
+            $inc: { totalDuration: req.body.duration },
+            //The $push operator appends a specified value to an array.
+            $push: { exercises: req.body }
+        },
+        { new: true }).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+
+});
 
 
 
