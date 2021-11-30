590a466820223c6c006749018bf1970804266993
Edgar Ortega <edort93@gmail.com>
2021-11-29 22:41:54 -0600

    added dependencies and export

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,16 @@ const router = require("express").Router();
 
 const Workout = require("../models/Workout.js");
 
-
-
-
-
+// route to add workouts to database
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+  });
 
 
 
