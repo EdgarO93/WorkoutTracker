3157fa049efa5f0e3ae656a6e09848460ce0dd65
Edgar Ortega <edort93@gmail.com>
2021-11-29 21:52:28 -0600

    added a app.use routes and exporting syntax

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -17,6 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/home.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
